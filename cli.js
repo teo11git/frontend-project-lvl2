91a8b770cb4b94751cef55c5553d9b006f8cb521
Teo <teobox11@gmail.com>
2021-01-28 22:17:33 +0300

    cleaning cli.js

--- cli.js
+++ cli.js
@@ -1,16 +1,18 @@
 import commander from 'commander';
 
 export default () => {
-  const { Command } = commander;
-  const program = new Command();
+  const { program } = commander;
 
   program
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
     .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0');
-  program.parse();
+    .version('1.0.0')
+      .action((filepath1, filepath2, options) => {
+        console.log(options);
+        console.log(`filepath: ${filepath1}`);
+        console.log(`filepath: ${filepath2}`);
+      });
 
-  const options = program.opts();
-  if (options.help) {
-    program.help();
-  }
+  program.parse();
 };
