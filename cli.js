0138e6566f29899f3363614afb528b669c427e8b
Teo <teobox11@gmail.com>
2021-01-29 03:17:12 +0300

    step forward

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 import commander from 'commander';
+import { default as makeDiff } from './index.js';
 
 export default () => {
   const { program } = commander;
@@ -9,9 +10,14 @@ export default () => {
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
       .action((filepath1, filepath2, options) => {
-        console.log(options);
         console.log(`filepath: ${filepath1}`);
         console.log(`filepath: ${filepath2}`);
+        const difference = makeDiff(filepath1, filepath2);
+
+       // console.log(options);
+       // console.log(`filepath: ${filepath1}`);
+       // console.log(`filepath: ${filepath2}`);
+        
       });
 
   program.parse();
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-const genDiff = () => {};
+import * as fs from 'fs';
+import * as path  from 'path';
+import * as _ from 'lodash';
+import { default as YAML } from 'js-yaml';
 
+const getFile = (filepath) => {
+  console.log(path.resolve(filepath));
+  const data = {
+    file: fs.readFileSync(filepath, 'utf-8'),
+    ext: path.parse(filepath).ext.slice(1),
+  };
+  return data;
+};
+
+const parser = {
+  json: JSON.parse,
+  yaml: YAML.load,
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = getFile(filepath1);
+  const data2 = getFile(filepath2);
+  const object1 = parser[data1.ext](data1.file);
+  const object2 = parser[data2.ext](data2.file);
+};
+// genDiff();
 export default genDiff;
