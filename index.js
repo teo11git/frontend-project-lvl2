cc680fa94af798189b09a478d6c380632c174b33
Teo <teobox11@gmail.com>
2021-01-30 02:49:39 +0300

    step forward

--- index.js
+++ index.js
@@ -1,12 +1,61 @@
 import * as fs from 'fs';
 import * as path  from 'path';
-import * as _ from 'lodash';
+import {default as _ } from 'lodash';
 import { default as YAML } from 'js-yaml';
+
+const makeMerge = (obj1, obj2) => {
+  const clone1 = _.cloneDeep(obj1);
+  const clone2 = _.cloneDeep(obj2);
+  _.merge(clone1, clone2);
+  return clone1;
+};
+
+const calculateDelta = (obj1, obj2, merged) => {
+ return  _.keys(merged).reduce((acc, key) => {
+    const hasNoValue = 'special_no_key'
+    const value1 = _.has(obj1, key) ? obj1[key] : hasNoValue;
+    const value2 = _.has(obj2, key) ? obj2[key] : hasNoValue;
+    if (value1 === hasNoValue) {
+      return acc = { ...acc,  [key]: [value2, 'added'] };
+    }
+    if (value2 === hasNoValue) {
+      return acc = { ...acc,  [key]: [value1, 'removed'] };
+    }
+    if (value1 === value2) {
+      return acc = { ...acc,  [key]: [value1, 'stay'] };
+    }
+    if (value1 !== value2) {
+      return acc = { ...acc,  [key]: [value1, value2, 'changed'] };
+    }
+  }, {});
+};
+
+const makeNormalOutput = (item) => {
+  const tab = '  ';
+  let [key, ...value] = item;
+  value = value[0];
+  const state = value[value.length - 1];
+  switch (state) {
+    case 'added':
+      return `${tab}+ ${key}: ${value[0]}\n`;
+    case 'removed':
+      return `${tab}- ${key}: ${value[0]}\n`;
+    case 'stay':
+      return `${tab}  ${key}: ${value[0]}\n`;
+    case 'changed':
+      return `${tab}- ${key}: ${value[0]}\n${tab}+ ${key}: ${value[1]}\n`;
+  }
+}
+
+const generateString = (obj) => {
+  const result = _.entries(obj).map(makeNormalOutput);
+  return `{\n${result.join('')}}`;
+}
 
 const getFile = (filepath) => {
   console.log(path.resolve(filepath));
   const data = {
-    file: fs.readFileSync(filepath, 'utf-8'),
+    content: fs.readFileSync(filepath, 'utf-8'),
     ext: path.parse(filepath).ext.slice(1),
   };
   return data;
@@ -20,8 +69,12 @@ const parser = {
 const genDiff = (filepath1, filepath2) => {
   const data1 = getFile(filepath1);
   const data2 = getFile(filepath2);
-  const object1 = parser[data1.ext](data1.file);
-  const object2 = parser[data2.ext](data2.file);
+  const object1 = parser[data1.ext](data1.content);
+  const object2 = parser[data2.ext](data2.content);
+  const merged = makeMerge(object1, object2);
+  const delta = calculateDelta(object1, object2, merged);
+  const resultString = generateString(delta);
+  console.log(resultString);
 };
 // genDiff();
 export default genDiff;
