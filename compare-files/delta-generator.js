7bad9d94c6f79be376069e405bad677f793fc4d8
Teo <teobox11@gmail.com>
2021-02-12 23:29:23 +0300

    change delta gen

--- compare-files/delta-generator.js
+++ compare-files/delta-generator.js
@@ -26,25 +26,20 @@ const calculateDelta = (obj1, obj2) => {
     const value1 = _.has(obj1, key) ? obj1[key] : hasNoKey;
     const value2 = _.has(obj2, key) ? obj2[key] : hasNoKey;
     const state = makeComparsion(value1, value2, hasNoKey);
-    if (!_.isPlainObject(value1) && !_.isPlainObject(value2)) {
-      switch (state) {
-        case 'added':
-          acc[key] = [value2, state];
-          break;
-        case 'changed':
-          acc[key] = [value1, value2, state];
-          break;
-        default:
-          acc[key] = [value1, state];
+    if (_.isPlainObject(value1) || _.isPlainObject(value2)) {
+      if (state === 'changed') {
+        acc[key] = calculateDelta(obj1[key], obj2[key]);
+      } else { 
+      acc[key] = ['[complex_value]', state];
       }
       return acc;
     }
-    switch (state) {
-      case 'changed':
-        acc[key] = calculateDelta(obj1[key], obj2[key], obj1[key]);
-        break;
-      default:
-        acc[key] = ['[complex_value]', state];
+    if (state === 'added') {
+      acc[key] = [value2, state];
+    } else if (state === 'changed') {
+      acc[key] = [value1, value2, state];
+    } else {
+      acc[key] = [value1, state];
     }
     return acc;
   }, {});
