493e27aafde6660bfec774444e56242197108db2
Teo <teobox11@gmail.com>
2021-02-16 02:33:19 +0300

    implemented new delta and test

--- __tests__/compare-files.delta-generator.test.js
+++ __tests__/compare-files.delta-generator.test.js
@@ -2,91 +2,22 @@ import { test, expect, describe } from '@jest/globals';
 import cloneDeep from 'lodash/cloneDeep';
 import makeDelta from '../compare-files/delta-generator.js';
 
-describe('Simple workig cases', () => {
-  test('should find removed names', () => {
-    const obj1 = { name: 'toRemove' };
-    const obj2 = {};
-    expect(makeDelta(obj1, obj2)).toEqual({ name: ['toRemove', 'removed'] });
-  });
-  test('should find added names', () => {
-    const obj1 = {};
-    const obj2 = { name: 'newValue' };
-    expect(makeDelta(obj1, obj2)).toEqual({ name: ['newValue', 'added'] });
-  });
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import genDiff from '../index.js';
 
-  test('should find keeped names', () => {
-    const obj1 = { name: 'value', secondName: 'toDelete' };
-    const obj2 = { name: 'value' };
-    expect(makeDelta(obj1, obj2)).toEqual({
-      name: ['value', 'stay'],
-      secondName: ['toDelete', 'removed'],
-    });
-  });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-  test('should find changed names', () => {
-    const obj1 = { name: 'toChange' };
-    const obj2 = { name: 'value' };
-    expect(makeDelta(obj1, obj2)).toEqual({ name: ['toChange', 'value', 'changed'] });
-  });
-});
-
-test('should work with plain objects', () => {
-  const obj1 = {
-    firstName: 'value',
-    secondName: 'toDelete',
-    thirdName: 'toChange',
-  };
-  const obj2 = cloneDeep(obj1);
-  delete obj2.secondName;
-  obj2.thirdName = 'newValue';
-  obj2.fourName = 'someValue';
-  const expectedObject = {
-    firstName: ['value', 'stay'],
-    secondName: ['toDelete', 'removed'],
-    thirdName: ['toChange', 'newValue', 'changed'],
-    fourName: ['someValue', 'added'],
-  };
-  expect(makeDelta(obj1, obj2)).toEqual(expectedObject);
-});
-
-test('should work with nasted objects', () => {
-  const obj1 = {
-    firstName: 'value',
-    secondName: {
-      name1: 'val',
-      name2: 'val2',
-    },
-    thirdName: {
-      name1: 'value1',
-      name2: {
-        name1: 'value1',
-        name2: 'value2',
-      },
-    },
-    fourName: {
-      name1: 'value1',
-    },
-  };
-
-  const obj2 = cloneDeep(obj1);
-  delete obj2.secondName;
-  obj2.thirdName.name2.name1 = 'someValue';
-  obj2.fourName.name2 = 'someValue2';
-
-  const expectedObject = {
-    firstName: ['value', 'stay'],
-    secondName: ['[complex_value]', 'removed'],
-    thirdName: {
-      name1: ['value1', 'stay'],
-      name2: {
-        name1: ['value1', 'someValue', 'changed'],
-        name2: ['value2', 'stay'],
-      },
-    },
-    fourName: {
-      name1: ['value1', 'stay'],
-      name2: ['someValue2', 'added'],
-    },
-  };
-  expect(makeDelta(obj1, obj2)).toEqual(expectedObject);
+test("should work with nasted data", () => {
+  const data1 = JSON.parse(readFile('nasted-file-1.json'));
+  const data2 = JSON.parse(readFile('nasted-file-2.json'));
+  // console.log('-----------------------');
+  // console.log(data1);
+  // console.log(data2);
+  const expectedAST = JSON.parse(readFile('nasted-ast.json'));
+  expect(makeDelta(data1, data2)).toEqual(expectedAST);
 });
--- compare-files/delta-generator.js
+++ compare-files/delta-generator.js
@@ -13,12 +13,13 @@ const makeComparsion = (val1, val2, specialValue) => {
   return 'changed';
 };
 
-const makeObject = (state, oldValue, newValue, children) => {
-  return {state, oldValue, newValue, children};
+const makeObject = (state, value, newValue, children) => {
+  return {state, value, newValue, children};
 };
 
 const isModifiedObjects = (value1, value2) => {
   if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+    // console.log('found Object!!!');
     return _.intersection(Object.keys(value1), Object.keys(value2))
             .length > 0;
   }
@@ -26,18 +27,30 @@ const isModifiedObjects = (value1, value2) => {
 };
 
 const calculateDelta = (obj1, obj2) => {
+  // console.log("----------------------");
+  // console.log(obj1);
+  // console.log(obj2);
   const specialValue = 'special_no_value';
   return _.union(Object.keys(obj1), Object.keys(obj2)).reduce((acc, key) => {
     const value1 = _.has(obj1, key) ? obj1[key] : specialValue;
     const value2 = _.has(obj2, key) ? obj2[key] : specialValue;
-    acc[key] = makeObject(
-     makeComparsion(val1, val2, specialValue),
-     value1,
-     value2,
-     isModifiedObjects(value1, value2)
-      ? calculateDelta(obj1[key], obj2[key])
-      : null
-    );
+    if (isModifiedObjects(obj1[key], obj2[key])) {
+      acc[key] = makeObject(
+        "changed",
+        "[Object]",
+        "[Object]",
+        calculateDelta(obj1[key], obj2[key])
+      );
+    } else {
+      // console.log(value1);
+      // console.log(value2);
+      acc[key] = makeObject(
+        makeComparsion(value1, value2, specialValue),
+        value1,
+        value2,
+        null
+      );
+    }
     return acc
   }, {});
 };
