b3f45afa2c508fbe0aaf2aaeb1fd4e7c5b4b54c6
Teo <teobox11@gmail.com>
2021-02-27 18:18:16 +0300

    deleted null cases in gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -12,11 +12,9 @@ const runCLI = () => {
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
     .action((filepath1, filepath2, options) => {
-      const difference = makeDiff(filepath1, filepath2, options.format);
-      if (difference === null) {
-        console.log('File is empty, or files is equal, or they don\'t have common keys');
-      }
-      console.log(difference);
+      console.log(
+        makeDiff(filepath1, filepath2, options.format)
+      );
     });
 
   program.parse();
