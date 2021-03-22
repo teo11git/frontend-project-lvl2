77988fb85661102717e446a25d8779fcf485e687
Teo <teobox11@gmail.com>
2021-03-22 04:21:43 +0300

    start working with stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,51 @@
 import _ from 'lodash';
 
+const indent = (depth, spaceCount = 4) => {
+  const indentCount = depth * spaceCount;
+  return '.'.repeat(indentCount < 0 ? 0 : indentCount);
+};
+
+const stringify = (data, depth) => {
+  console.log('start stringify');
+  console.log(data);
+  if(!(_.isPlainObject(data))) {
+    console.log('Just write data');
+    return `${data}`;
+  };
+  console.log('start render nested object');
+  const result = Object.entries(data).map(([key, value]) => `${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
+  return `/\n${result.join('\n')}\n${indent(depth)}>>/`  
+};
+
+const mapping = {
+  added: (node, depth) => {
+    console.log('from added');
+    console.log(node.newValue);
+    return `${indent(depth)}+ ${node.key}: ${stringify(node.newValue, depth)}\n`
+  },
+  removed: (node, depth) => `${indent(depth)}- ${node.key}: ${stringify(node.oldValue, depth)}\n`,
+  changed: (node, depth) => `${indent(depth)}- ${node.key}: ${stringify(node.oldValue, depth)}\n${indent(depth)}+ ${node.key}: ${stringify(node.newValue, depth)}\n`,
+  unchanged: (node, depth) => `${indent(depth)}!!${node.key}: ${stringify(node.oldValue, depth)}\n`,
+  nasted: (node, depth, iter) => {
+    return `${indent(depth)}  ${node.key}: $\n${iter(node.children, depth + 1)}${indent(depth)}<<$\n`;    
+  },
+};
+
+const renderTree = (ast) => {
+  const iter = (tree, depth) => {
+    console.log('Start!!!');
+    const result = tree.map((node) => {
+      console.log(node);
+      return mapping[node.type](node, depth, iter);
+    });
+    return `${result.join('')}`;
+  };
+  return `{\n${iter(ast, 0)}}`;
+};
+
+export default renderTree;
+
+/*
 const makeTabs = (indent) => `${' '.repeat(indent)}`;
 
 const renderObject = (item, indent) => Object.entries(item).map(([key, value]) => {
@@ -41,3 +87,4 @@ const prettify = (data, indent = 2) => (
 );
 
 export default (data) => `{\n${prettify(data).slice(0, -1)}\n}`;
+*/
