23848c8a36f706e74c1901fc3f51a337491fa808
Teo <teobox11@gmail.com>
2021-03-23 03:20:46 +0300

    modified build tree

--- src/buildTree.js
+++ src/buildTree.js
@@ -3,34 +3,32 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => _(_.union(Object.keys(data1), Object.keys(data2)))
   .sortBy()
   .map((key) => {
-    const oldValue = _.has(data1, key) ? data1[key] : undefined;
-    const newValue = _.has(data2, key) ? data2[key] : undefined;
-    if (oldValue === undefined) {
+    if (!_.has(data1, key)) {
       return {
-        type: 'added', key, oldValue, newValue,
+        type: 'added', key, newValue: data2[key],
       };
     }
-    if (newValue === undefined) {
+    if (!_.has(data2, key)) {
       return {
-        type: 'removed', key, oldValue, newValue,
+        type: 'removed', key, oldValue: data1[key],
       };
     }
-    if (_.isEqual(oldValue, newValue)) {
+    if (_.isEqual(data1[key], data2[key])) {
       return {
-        type: 'unchanged', key, oldValue, newValue,
+        type: 'unchanged', key, oldValue: data1[key],
       };
     }
-    if (_.isPlainObject(oldValue) && _.isPlainObject(newValue)) {
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
       return {
         type: 'nested',
         key,
-        oldValue,
-        newValue,
+        oldValue: data1[key],
+        newValue: data2[key],
         children: [...buildTree(data1[key], data2[key])],
       };
     }
     return {
-      type: 'changed', key, oldValue, newValue,
+      type: 'changed', key, oldValue: data1[key], newValue: data2[key],
     };
   });
 export default buildTree;
